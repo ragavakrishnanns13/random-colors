30a07501000a8c3f65d1d11fcdad6fb13e1a85f9
ragavakrishnanns <210701201@rajalakshmi.edu.in>
2023-04-25 06:51:38 +0530

    commiting playgrounds

---
+++ test1.swiftpm/test1.swift
@@ -0,0 +1,12 @@
+//
+//  test1.swift
+//  test1
+//
+//  Created by Ragava Krishnan N S on 25/04/23.
+//
+
+import UIKit
+
+var ages = [22,23,25,36]
+print(ages
+)
---
+++ testPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//var ages = [11,22,33,44,55]
+//
+//ages.insert(29, at: 0
+//)
+//ages.append(24)
+//print(ages)
+//ages.sort()
+//print(ages)
+
+var ages = [1,2,3,4,5,6,7]
+
+var agesSet: Set<Int>=[]
+agesSet = [22,33,44,55,66,55,66,66,66,66]
+agesSet.insert(12)
+agesSet.contains(101)
+print(agesSet)
+
+
+
