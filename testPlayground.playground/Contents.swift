3d16f42cf52ee5659de16b978f01b84f0ecbe103
ragavakrishnanns <210701201@rajalakshmi.edu.in>
2023-04-26 07:03:10 +0530

    commiting 21/2

--- testPlayground.playground/Contents.swift
+++ testPlayground.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//arrays:
 //var ages = [11,22,33,44,55]
 //
 //ages.insert(29, at: 0
@@ -9,13 +9,50 @@ import UIKit
 //ages.sort()
 //print(ages)
 
-var ages = [1,2,3,4,5,6,7]
+//sets
+//var ages = [1,2,3,4,5,6,7]
+//
+//var agesSet: Set<Int>=[]
+//agesSet = [22,33,44,55,66,55,66,66,66,66]
+//agesSet.insert(12)
+//agesSet.contains(101)
+//print(agesSet)
 
-var agesSet: Set<Int>=[]
-agesSet = [22,33,44,55,66,55,66,66,66,66]
-agesSet.insert(12)
-agesSet.contains(101)
-print(agesSet)
+//dictionary
+//let children: [String:String]=[
+//    "Crackhead":"Luke",
+//    "Smart":"Alex",
+//    "Flirty":"Megan"
+//]
+//children["Smart"]
 
+//functions, argument label, call site, parameters
+//func adder(firstNum:Int, to secondNum:Int)->Int{
+//    return firstNum+secondNum;
+//}
+//
+//adder(firstNum: 33, to: 34)
+//
 
+//if,else if , else
+//let darkModeon=false
+//
+//if darkModeon==true{
+//    print("Epic")
+//}
+//else{
+//    print("Not epic bro")
+//}
+
+//for loops, _ (used in place of immutable variable), ranges ... from to, ..< from to lessthan, ..>from to greter than
+//
+//var randomIntegers:[Int] = []
+//
+//for _ in 0..<10 {
+//    let randomNumbers = Int.random(in: 0...50)
+//    randomIntegers.append(randomNumbers)
+//}
+//print(randomIntegers)
+
+//Enum or Enumeration
 
